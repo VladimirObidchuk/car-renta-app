1340b18878f698819b485d39d93dbb53827b5c60
Vladimir <kremench@gmail.com>
2025-09-13 22:59:54 +0300

    add navigation for logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,11 @@
+import { NavLink } from "react-router-dom";
 import Icon from "../Icon/Icon";
 import style from "./Logo.module.css";
 
 export default function Logo() {
   return (
-    <div>
+    <NavLink to="/">
       <Icon name="logo" classname={style.logoIcon} width={104} height={16} />
-    </div>
+    </NavLink>
   );
 }
